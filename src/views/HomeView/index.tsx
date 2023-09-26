c49d5f8e6dfe7eebfaea96063772ee5611bb3a11
purrnama <purrnama@proton.me>
2023-09-26 19:48:22 +0800

    Task card component

--- src/views/HomeView/index.tsx
+++ src/views/HomeView/index.tsx
@@ -3,6 +3,7 @@ import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { SolanaLogo } from "components";
 import styles from "./index.module.css";
+import TaskCard from "components/TaskCard";
 
 export const HomeView: FC = ({}) => {
   const { publicKey } = useWallet();
@@ -16,33 +17,34 @@ export const HomeView: FC = ({}) => {
           <div className="flex-1 px-2 mx-2">
             <span className="text-lg font-bold">Taskaru</span>
           </div>
-          
+
           {/* Added Button code here*/}
-          <div className = "flex-none">
-            <button class="btn btn-ghost">Taskboard</button>
-          </div>
-          <div className = "flex-none">
-            <button class="btn btn-ghost">Task Listing</button>
-          </div>
-          <div className = "flex-none">
-            <button class="btn btn-ghost">Portfolio</button>
+          <div className="flex-none">
+            <button className="btn btn-ghost">Taskboard</button>
           </div>
           <div className="flex-none">
-          <WalletMultiButton style={{ backgroundColor: "#FF4874" }} className="btn btn-ghost" />
+            <button className="btn btn-ghost">Task Listing</button>
           </div>
-
+          <div className="flex-none">
+            <button className="btn btn-ghost">Portfolio</button>
+          </div>
+          <div className="flex-none">
+            <WalletMultiButton
+              style={{ backgroundColor: "#FF4874" }}
+              className="btn btn-ghost"
+            />
+          </div>
         </div>
 
         <div className="text-center pt-2">
           <div className="hero min-h-16 py-4">
             <div className="text-center hero-content">
               <div className="max-w-lg">
-                <h1 className="mb-5 text-5xl font-bold">
-                  Start working now.
-                </h1>
+                <h1 className="mb-5 text-5xl font-bold">Start working now.</h1>
                 <p className="mb-5">
-                Explore hundreds of tasks from open-source projects waiting for you to be completed.
-Harness the speed and creativity of hackathons to rapidly develop and earn for your contributions. 
+                  Explore hundreds of tasks from open-source projects waiting
+                  for you to be completed. Harness the speed and creativity of
+                  hackathons to rapidly develop and earn for your contributions.
                 </p>
                 <p>
                   {publicKey ? <>Your address: {publicKey.toBase58()}</> : null}
@@ -51,30 +53,16 @@ Harness the speed and creativity of hackathons to rapidly develop and earn for y
             </div>
           </div>
         </div>
-        
-        <h1>
-          Active tasks
-        </h1>
 
-        <div class="card w-96 bg-base-100 shadow-xl">
-            <div class="card-actions justify-end">
-              <button class="btn btn-square btn-sm">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
-              </button>
-            </div>
-            <div class="card-body">
-              <h2 class="card-title">Latte Protocol</h2>
-              <p>New Component Implementation</p>
-              <div class="badge badge-neutral mr-2 mb-2">Next.js</div>
-              <div class="badge badge-neutral mr-2 mb-2">Typescript</div>
-              <progress class="progress w-56" value="10" max="100"></progress>
-              
-            </div>
-            
+        <h1>Active tasks</h1>
+        <div className="flex flex-row gap-4">
+          <TaskCard
+            title="Latte Protocol"
+            description="New Component Implementation"
+          />
+          <TaskCard title="Matcha Client" description="Fix issues" />
         </div>
       </div>
     </div>
   );
 };
-
-
