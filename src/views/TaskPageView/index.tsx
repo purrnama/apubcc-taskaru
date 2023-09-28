2086541dac3ed740ff43bdbf3c222a6c3adc4728
Brutt09 <leonleebrit@gmail.com>
2023-09-28 17:50:55 +0800

    all done, juz the size not so correct

--- src/views/TaskPageView/index.tsx
+++ src/views/TaskPageView/index.tsx
@@ -26,7 +26,7 @@ export const TaskPageView: FC = ({}) => {
                   <div className="badge badge-accent"> Active </div>
                   <p className="gap-2 mb-5"> Task opened 3 days ago </p>
                   <div className=" mb-2" style={{ marginLeft: "270px" }}>
-                    <div className="badge badge-outline">View on Github</div>
+                    <button className="btn btn-outline">View on Github</button>
                   </div>
                 </div>
               </div>
@@ -38,27 +38,57 @@ export const TaskPageView: FC = ({}) => {
           <div className="card-body">
             <div className="grid grid-cols-5 gap-6">
               <div className="col-span-3 card w-30 mb-5 bg-white bg-opacity-10 ">
+                
                 <div className="card-body">
-                  <h2 className="card-title">Issue Description: </h2>
-                  <p>
+                  <h2 className="card-title  mt-5 mb-5">Issue Description: </h2>
+                  <p className="justify-text">
                     We are looking to enhance our website by adding a new React
                     component, [Component Name], to improve the user experience
                     and provide additional functionality. This component will
                     [briefly describe the purpose and functionality of the
                     component].
                   </p>
-                  <h2 className="mr-2 mb-2 card-title">Acceptance Criteria:</h2>
-                  <p>
-                    - The new component should be fully funrtional and free of
-                    any errors or bugs.
-                  </p>
+
+                  <h2 className="card-title mt-5 mb-5">Expected Behaviour Criteria:</h2>
+                  <p> • When [describe how the component should behave], it should [describe the expected behavior].</p>
+                  <p className="justify-text"> • [Add any other specific expected behaviors here].</p>
+                  <h2 className="justify-text mt-5 mb-5"> Designs and Mockups: [ applicable, provide links or attachments to any design mockups or wireframes that illustrate how the new component should look and function].</h2>
+
+
+                  <h2 className="card-title mt-5 mb-5">Implementation Details:</h2>
+                  <p>• Create a new React component named (Component Name).</p>
+                  <p className="justify-text">• Integrate the component into the appropriate page(s) within the website.</p>
+                  <p className="justify-text">• Ensure that the component is responsive and functions seamlessly across different devices and screen sizes.</p>
+                  <p className="justify-text">• Implement any necessary CSS or styling to match the website's design.</p>
+
+                  
+
+                  <h2 className="card-title mt-5 mb-5">Acceptance Criteria:</h2>
+
+                  <p className="justify-text">• The new component should be fully functional and free of any errors or bugs.</p>
+                  <p className="justify-text">• It should be well-documented, including clear comments and usage instructions.</p>
+                  <p className="justify-text">• The component's code should adhere to our coding standards and best practices.</p>
+                  <p className="justify-text">• The component should be visually consistent with the rest of the website.</p>
+
+
+
                 </div>
               </div>
               <div className="col-span-2 flex flex-col gap-2">
-                <div className="card w-30 mb-5 bg-white bg-opacity-10 ">
-                  <div className="card-body">
-                    <h2 className="card-title">Bounty</h2>
-                    <h2 className="card-title mb-8">Paricipants</h2>
+                <div className="card mb-2 bg-white bg-opacity-10 ">
+                  <div className="card-body ">
+                    <div className="flex flex-row gap-6">
+                      <h2 className="card-title">Bounty</h2>
+                      <h3 className="text-xl-1 ml-20 font-display font-normal text-white">
+                        700 USDC
+                      </h3>
+                    </div>
+                    <div className="flex flex-row gap-6">
+                      <h2 className="card-title mb-8">Paricipants</h2>
+                      <h2 className="text-xl-1 ml-24 font-display font-normal text-white">
+                        8
+                      </h2>
+                    </div>
                     <h2 className="mr-2 mb-2 card-title">Submission period</h2>
                     <p>12 Sep 2030 - 26 Sep 2030</p>
                     <progress
@@ -68,7 +98,7 @@ export const TaskPageView: FC = ({}) => {
                     ></progress>
                     <p className="mb-4">14 days remaining</p>
                     {publicKey && (
-                      <div className="card mb-4 w-30 bg-white bg-opacity-10 ">
+                      <div className="card mb-5 h-40 w-30 bg-white bg-opacity-10 ">
                         <div className="flex flex-row">
                           <div className="card-body">
                             <p>Participating as</p>
@@ -110,10 +140,15 @@ export const TaskPageView: FC = ({}) => {
                     </button>
                   </div>
                 </div>
-                <div className="card w-120 h-300 bg-white bg-opacity-10 bg-base-80 shadow-xl">
+                <div className="card w-120 bg-white bg-opacity-10 bg-base-80 shadow-xl">
                   <div className="card-body">
                     <h2 className="card-title">Task Resources</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div className="flex flex-row">
+                      <button className="btn btn-block btn-outline bg-white bg-opacity-10">Github Source Code</button>
+                    </div>
+                    <div className="flex flex-row">
+                      <button className="btn btn-block btn-outline bg-white bg-opacity-10">Latte Protocol Documentaion</button>
+                    </div>
                   </div>
                 </div>
               </div>
