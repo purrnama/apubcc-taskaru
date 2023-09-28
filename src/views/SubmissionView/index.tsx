0202c83fde3dfbdebde38641c1bd265710870cdb
purrnama <purrnama@proton.me>
2023-09-28 17:23:03 +0800

    feat: input logic

--- src/views/SubmissionView/index.tsx
+++ src/views/SubmissionView/index.tsx
@@ -1,11 +1,15 @@
-import { FC } from "react";
+import { ChangeEvent, FC, useState } from "react";
 import { useWallet } from "@solana/wallet-adapter-react";
 import TaskCard from "components/TaskCard";
 import Header from "components/Header";
 
 export const SubmissionView: FC = ({}) => {
   const { publicKey } = useWallet();
-  const onClick = () => {};
+  const [value, setValue] = useState("");
+
+  const onChangeValue = (event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
 
   return (
     <div className="container mx-auto max-w-6xl p-8 2xl:px-0">
@@ -21,13 +25,11 @@ export const SubmissionView: FC = ({}) => {
                 <div className="flex flex-row gap-8 pt-2 mb-4">
                   <div className="badge badge-accent"> Active </div>
                   <p className="gap-2 mb-5"> Task opened 3 days ago </p>
-                    <div className=" mb-2" style={{ marginLeft: "270px" }}>
-                      <div className="badge badge-outline">View on Github</div>
-                    </div>
+                  <div className=" mb-2" style={{ marginLeft: "270px" }}>
+                    <div className="badge badge-outline">View on Github</div>
+                  </div>
                 </div>
-                <p>
-                  {publicKey ? <>Your ID: {publicKey.toBase58()}</> : null}
-                </p>
+                <p>{publicKey ? <>Your ID: {publicKey.toBase58()}</> : null}</p>
               </div>
             </div>
           </div>
@@ -35,23 +37,31 @@ export const SubmissionView: FC = ({}) => {
         <div className="container max-w-2xl mx-auto">
           <div className="card h-50 bg-white bg-opacity-10 shadow-xl">
             <div className="card-body flex flex-col gap-4">
-            <div className="flex flex-row justify-between">
+              <div className="flex flex-row justify-between">
                 <div>
-                  <h1 className="text-3xl font-display font-normal text-white mb-16">Submit solution</h1>
-                </div>
-                <div className="flex items starts">
-                  <button className="btn btn-square btn-sm">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="black">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                  </button>
+                  <h1 className="text-3xl font-display font-normal text-white mb-16">
+                    Submit solution
+                  </h1>
                 </div>
               </div>
               <p className="text-gray-400 text-xl">Pull request link</p>
-              <div>
-                <input type="link" placeholder="https://github.com/latte/monorepo/pull/1460" className="input input-bordered input-md w-full max-w-2xl" />
+              <div onChange={onChangeValue}>
+                <input
+                  type="link"
+                  value={value}
+                  placeholder=""
+                  className="input input-bordered input-md w-full max-w-2xl"
+                />
               </div>
-              <button className="btn btn-outline btn-accent btn-block">Comfirm submission</button>
+              <button
+                disabled={!Boolean(value)}
+                className={
+                  "btn btn-block " +
+                  (Boolean(value) ? "btn-success" : "btn-disabled")
+                }
+              >
+                Confirm submission
+              </button>
             </div>
           </div>
         </div>
