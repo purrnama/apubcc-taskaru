f2c7d784966fbc5fc10207a407b037d16636a9ab
purrnama <purrnama@proton.me>
2023-09-28 15:02:54 +0800

    feat: improve task page

--- src/views/TaskPageView/index.tsx
+++ src/views/TaskPageView/index.tsx
@@ -2,16 +2,14 @@ import { FC } from "react";
 import { useWallet } from "@solana/wallet-adapter-react";
 import TaskCard from "components/TaskCard";
 import Header from "components/Header";
+import Image from "next/image";
 
 export const TaskPageView: FC = ({}) => {
   const { publicKey } = useWallet();
 
   const onClick = () => {};
 
-
-//<p style="text-align: left;">This text is aligned to the left.</p>
-
-
+  //<p style="text-align: left;">This text is aligned to the left.</p>
 
   return (
     <div className="container mx-auto max-w-6xl p-8 2xl:px-0">
@@ -27,13 +25,10 @@ export const TaskPageView: FC = ({}) => {
                 <div className="flex flex-row gap-8 pt-2 mb-4">
                   <div className="badge badge-accent"> Active </div>
                   <p className="gap-2 mb-5"> Task opened 3 days ago </p>
-                    <div className=" mb-2" style={{ marginLeft: "270px" }}>
-                      <div className="badge badge-outline">View on Github</div>
-                    </div>
+                  <div className=" mb-2" style={{ marginLeft: "270px" }}>
+                    <div className="badge badge-outline">View on Github</div>
+                  </div>
                 </div>
-                <p>
-                  {publicKey ? <>Your ID: {publicKey.toBase58()}</> : null}
-                </p>
               </div>
             </div>
           </div>
@@ -45,39 +40,74 @@ export const TaskPageView: FC = ({}) => {
               <div className="card w-30 mb-5 bg-white bg-opacity-10 ">
                 <div className="card-body">
                   <h2 className="card-title">Issue Description: </h2>
-                  <h2 className="card-title">Expected Behaviour: </h2>
-                  <h2 className="mr-2 mb-2 card-title">Implementation Details:</h2>
+                  <p>
+                    We are looking to enhance our website by adding a new React
+                    component, [Component Name], to improve the user experience
+                    and provide additional functionality. This component will
+                    [briefly describe the purpose and functionality of the
+                    component].
+                  </p>
                   <h2 className="mr-2 mb-2 card-title">Acceptance Criteria:</h2>
-                  <p>- The new component should be fully funrtional and free of any errors or bugs.</p>
+                  <p>
+                    - The new component should be fully funrtional and free of
+                    any errors or bugs.
+                  </p>
                 </div>
               </div>
-              <div className="flex flex-col gap-5">
+              <div className="flex flex-col gap-2">
                 <div className="card w-30 mb-5 bg-white bg-opacity-10 ">
                   <div className="card-body">
                     <h2 className="card-title">Bounty</h2>
                     <h2 className="card-title mb-8">Paricipants</h2>
                     <h2 className="mr-2 mb-2 card-title">Submission period</h2>
                     <p>12 Sep 2030 - 26 Sep 2030</p>
-                    <progress className="progress w-56" value="75" max="150"></progress>
-                    <p>14 days remaining</p>
-                    <div className="card mb-5 w-30 bg-white bg-opacity-10 ">
-                      <div className="flex flex-row">
-                        <div className="card-body">
-                          <p>Participating as</p>
-                          <div className="flex flex-row gap-2 avatar placeholder">
-                            <div className="bg-neutral-focus mt-5 text-neutral-content rounded-full w-12 h-12">
-                              <img
-                                src="https://www.wikihow.com/images/f/ff/Draw-a-Cute-Cartoon-Person-Step-14.jpg"
-                                alt="Avatar"
-                                className="w-12 h-12 rounded-full object-cover"
-                              />
+                    <progress
+                      className="progress w-56"
+                      value="75"
+                      max="150"
+                    ></progress>
+                    <p className="mb-4">14 days remaining</p>
+                    {publicKey && (
+                      <div className="card mb-4 w-30 bg-white bg-opacity-10 ">
+                        <div className="flex flex-row">
+                          <div className="card-body">
+                            <p>Participating as</p>
+                            <div className="flex flex-row items-center gap-2 avatar placeholder">
+                              <div className="bg-neutral-focus mt-5 text-neutral-content rounded-full">
+                                <Image
+                                  src={"/default-avatar.png"}
+                                  alt="Avatar"
+                                  width={60}
+                                  height={60}
+                                  className="rounded-full"
+                                />
+                              </div>
+                              <h2 className="text-xl font-display font-normal text-white">
+                                @Dev
+                              </h2>
+                              <h3 className="text-gray-200">
+                                {publicKey ? (
+                                  <>
+                                    Your address:{" "}
+                                    {publicKey.toBase58().slice(0, 8) + "..."}
+                                  </>
+                                ) : (
+                                  "0xaddr...xyz"
+                                )}
+                              </h3>
                             </div>
-                            <h2 className="text-1xl mr-4 mt-5 font-display font-normal text-white">@Dev</h2>
-                            <h3 className="text-gray-200 mt-7 mb-5">0xaddr...xyz</h3>
                           </div>
                         </div>
                       </div>
-                    </div>
+                    )}
+
+                    <button
+                      className={
+                        "btn " + (publicKey ? "btn-success" : "btn-disabled")
+                      }
+                    >
+                      {publicKey ? "Accept Task" : "Connect Wallet"}
+                    </button>
                   </div>
                 </div>
                 <div className="card w-120 h-300 bg-white bg-opacity-10 bg-base-80 shadow-xl">
@@ -90,20 +120,6 @@ export const TaskPageView: FC = ({}) => {
             </div>
           </div>
         </div>
-
-        <div className="flex flex-row gap-4 my-4">
-          <TaskCard
-            title="Latte Protocol"
-            description="New Component Implementation"
-          />
-          <TaskCard title="Matcha Client" description="Fix issues" />
-        </div>
-
-
-
-
-
-
       </div>
     </div>
   );
