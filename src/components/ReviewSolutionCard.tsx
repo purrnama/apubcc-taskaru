50ae931cd9fd8ab97931898703daf9dec9b52789
purrnama <purrnama@proton.me>
2023-09-28 17:07:30 +0800

    fix: make radio groups independent

--- src/components/ReviewSolutionCard.tsx
+++ src/components/ReviewSolutionCard.tsx
@@ -1,26 +1,37 @@
-
 export default function ReviewSolutionCard({
   noSolution,
-
 }: {
   noSolution: number;
-
 }) {
-
   return (
-  <div className="card w-120 h-300 bg-white bg-opacity-10 bg-base-80 shadow-xl">
-    <div className="card-body">
-      <div className="flex justify-between ...">
-        <h2 className="card-title">Solution {noSolution}</h2>
-        <div className="card w-30 h-300 bg-white bg-opacity-5 bg-base-80 shadow-xl">
-          <div className="btn-group">
-            <input type="radio" name="options" data-title="Poor" className="btn" />
-            <input type="radio" name="options" data-title="Good" className="btn" />
-            <input type="radio" name="options" data-title="Great" className="btn" />
+    <div className="card w-120 h-300 bg-white bg-opacity-10 bg-base-80 shadow-xl">
+      <div className="card-body">
+        <div className="flex justify-between ...">
+          <h2 className="card-title">Solution {noSolution}</h2>
+          <div className="card w-30 h-300 bg-white bg-opacity-5 bg-base-80 shadow-xl">
+            <div className="btn-group">
+              <input
+                type="radio"
+                name={"options" + noSolution}
+                data-title="Poor"
+                className="btn"
+              />
+              <input
+                type="radio"
+                name={"options" + noSolution}
+                data-title="Good"
+                className="btn"
+              />
+              <input
+                type="radio"
+                name={"options" + noSolution}
+                data-title="Great"
+                className="btn"
+              />
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
   );
 }
