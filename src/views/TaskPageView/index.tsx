df13dd967eb20f9400f777938245bccc805957bb
Brutt09 <leonleebrit@gmail.com>
2023-09-28 18:00:39 +0800

    all done, left a box size not correct

--- src/views/TaskPageView/index.tsx
+++ src/views/TaskPageView/index.tsx
@@ -24,7 +24,7 @@ export const TaskPageView: FC = ({}) => {
                 </h1>
                 <div className="flex flex-row gap-8 pt-2 mb-4">
                   <div className="badge badge-accent"> Active </div>
-                  <p className="gap-2 mb-5"> Task opened 3 days ago </p>
+                    <p className="gap-2 mb-5"> Task opened 3 days ago </p>
                   <div className=" mb-2" style={{ marginLeft: "270px" }}>
                     <button className="btn btn-outline">View on Github</button>
                   </div>
@@ -38,40 +38,29 @@ export const TaskPageView: FC = ({}) => {
           <div className="card-body">
             <div className="grid grid-cols-5 gap-6">
               <div className="col-span-3 card w-30 mb-5 bg-white bg-opacity-10 ">
-                
                 <div className="card-body">
                   <h2 className="card-title  mt-5 mb-5">Issue Description: </h2>
-                  <p className="justify-text">
-                    We are looking to enhance our website by adding a new React
-                    component, [Component Name], to improve the user experience
-                    and provide additional functionality. This component will
-                    [briefly describe the purpose and functionality of the
-                    component].
-                  </p>
-
+                    <p className="justify-text">
+                      We are looking to enhance our website by adding a new React
+                      component, [Component Name], to improve the user experience
+                      and provide additional functionality. This component will
+                      [briefly describe the purpose and functionality of the
+                      component].
+                    </p>
                   <h2 className="card-title mt-5 mb-5">Expected Behaviour Criteria:</h2>
-                  <p> • When [describe how the component should behave], it should [describe the expected behavior].</p>
-                  <p className="justify-text"> • [Add any other specific expected behaviors here].</p>
-                  <h2 className="justify-text mt-5 mb-5"> Designs and Mockups: [ applicable, provide links or attachments to any design mockups or wireframes that illustrate how the new component should look and function].</h2>
-
-
+                    <p> • When [describe how the component should behave], it should [describe the expected behavior].</p>
+                    <p className="justify-text"> • [Add any other specific expected behaviors here].</p>
+                    <h2 className="justify-text mt-5 mb-5"> Designs and Mockups: [ applicable, provide links or attachments to any design mockups or wireframes that illustrate how the new component should look and function].</h2>
                   <h2 className="card-title mt-5 mb-5">Implementation Details:</h2>
-                  <p>• Create a new React component named (Component Name).</p>
-                  <p className="justify-text">• Integrate the component into the appropriate page(s) within the website.</p>
-                  <p className="justify-text">• Ensure that the component is responsive and functions seamlessly across different devices and screen sizes.</p>
-                  <p className="justify-text">• Implement any necessary CSS or styling to match the website's design.</p>
-
-                  
-
+                    <p>• Create a new React component named (Component Name).</p>
+                    <p className="justify-text">• Integrate the component into the appropriate page(s) within the website.</p>
+                    <p className="justify-text">• Ensure that the component is responsive and functions seamlessly across different devices and screen sizes.</p>
+                    <p className="justify-text">• Implement any necessary CSS or styling to match the website's design.</p>
                   <h2 className="card-title mt-5 mb-5">Acceptance Criteria:</h2>
-
-                  <p className="justify-text">• The new component should be fully functional and free of any errors or bugs.</p>
-                  <p className="justify-text">• It should be well-documented, including clear comments and usage instructions.</p>
-                  <p className="justify-text">• The component's code should adhere to our coding standards and best practices.</p>
-                  <p className="justify-text">• The component should be visually consistent with the rest of the website.</p>
-
-
-
+                    <p className="justify-text">• The new component should be fully functional and free of any errors or bugs.</p>
+                    <p className="justify-text">• It should be well-documented, including clear comments and usage instructions.</p>
+                    <p className="justify-text">• The component's code should adhere to our coding standards and best practices.</p>
+                    <p className="justify-text">• The component should be visually consistent with the rest of the website.</p>
                 </div>
               </div>
               <div className="col-span-2 flex flex-col gap-2">
