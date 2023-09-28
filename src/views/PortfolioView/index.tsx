e93ff9a7d34d3712d4a4dacf5a71817606895a04
purrnama <purrnama@proton.me>
2023-09-28 14:28:32 +0800

    feat: improve portfolio page

--- src/views/PortfolioView/index.tsx
+++ src/views/PortfolioView/index.tsx
@@ -2,9 +2,9 @@ import { FC } from "react";
 import { useWallet } from "@solana/wallet-adapter-react";
 import TaskCard from "components/TaskCard";
 import Header from "components/Header";
+import Image from "next/image";
 
 export const PortfolioView: FC = ({}) => {
-
   const { publicKey } = useWallet();
   const onClick = () => {};
 
@@ -12,75 +12,98 @@ export const PortfolioView: FC = ({}) => {
     <div className="container mx-auto max-w-6xl p-8 2xl:px-0">
       <div>
         <Header />
-        <div className="pt-1">
+        <div className="pt-2">
           <div className="hero min-h-16 py-4">
             <div className="hero-content">
-              <div className="max-w-3xl">
-                <h1 className="text-3xl font-display mt-4"> 
-                  Welcome back!
-                </h1>
-                <p>
-                  {publicKey ? <>Your address: {publicKey.toBase58()}</> : null}
-                </p>
-              </div>
+              <div className="max-w-3xl"></div>
             </div>
           </div>
         </div>
-        <div className="card h-50 w-70 bg-white bg-opacity-10 shadow-xl">
-          <div className="card-body h-80 w-80">
-            <div className="flex mb-10">
-              <div className="flex flex-col gap-8 avatar placeholder">
-                <div className="bg-neutral-focus text-neutral-content rounded-full ring ring-white ring-offset-base-100 -ring-offset-2 w-24 h-24">
-                  <img
-                    src="https://www.wikihow.com/images/f/ff/Draw-a-Cute-Cartoon-Person-Step-14.jpg"
+        <div className="container max-w-5xl mx-auto">
+          <h1 className="mb-4 text-4xl font-display">Welcome back!</h1>
+          <div className="card h-50 bg-white bg-opacity-10 shadow-xl">
+            <div className="card-body">
+              <div className="grid grid-cols-3">
+                <div className="">
+                  <Image
+                    src={"/default-avatar.png"}
                     alt="Avatar"
-                    className="w-24 h-24 rounded-full object-cover"
+                    width={128}
+                    height={128}
+                    className="rounded-full"
                   />
+                  <h2 className="text-3xl font-display font-normal text-white">
+                    @Dev
+                  </h2>
+                  <h3 className="text-gray-200">
+                    {publicKey ? (
+                      <>Your address: {publicKey.toBase58()}</>
+                    ) : (
+                      "0xaddr...xyz"
+                    )}
+                  </h3>
                 </div>
-                <h2 className="text-1xl font-display font-normal text-white">@Dev</h2>
-                <h3 className="text-gray-200 -mt-8">0xaddr...xyz</h3>
-              </div>
-              <div className="flex flex-row items-center ml-12 -mt-24"> 
-              </div>
-                <div className="avatar placeholder ml-4"> 
-                  <div className="bg-pink-500 text-neutral-content rounded-full border border-black w-6 h-6 ml-2">
-                    <span className="text-s font-display font-normal text-black">T</span>
+                <div className="col-span-2 flex flex-col gap-8">
+                  <div className="flex flex-row gap-4">
+                    <div className="flex flex-row items-center">
+                      <div className="avatar placeholder">
+                        <div className="bg-pink-500 text-neutral-content rounded-full w-8 h-8 ml-2">
+                          <span className="text-xs font-display">T</span>
+                        </div>
+                      </div>
+                      <div className="ml-4">
+                        <p className="text-gray-400 text-xl">810</p>
+                      </div>
+                    </div>
+                    <div className="flex flex-row items-center">
+                      <div className="avatar placeholder">
+                        <div className="bg-pink-500 text-neutral-content rounded-full w-8 h-8 ml-2">
+                          <span className="text-xs font-display">R</span>
+                        </div>
+                      </div>
+                      <div className="ml-4">
+                        <p className="text-gray-400 text-xl">2600</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="grid grid-cols-3 gap-4">
+                    <div className="flex flex-col gap-2">
+                      <h3 className="text-xl font-display font-normal text-white">
+                        Tasks completed
+                      </h3>
+                      <p className="font-sans text-xl">8</p>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <h3 className="text-xl font-display font-normal text-white">
+                        Projects involved
+                      </h3>
+                      <div className="flex flex-row gap-2">
+                        <div className="w-8 h-8 rounded-md bg-green-400"></div>
+                        <div className="w-8 h-8 rounded-md bg-yellow-300"></div>
+                      </div>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <h3 className="text-xl font-display font-normal text-white">
+                        Total earned
+                      </h3>
+                      <p className="font-sans text-xl">1600 USDC</p>
+                    </div>
+                  </div>
+                  <div>
+                    <h1 className="font-display text-xl">Contributions</h1>
+                    <div className="w-full h-[100px] bg-neutral bg-opacity-10"></div>
                   </div>
                 </div>
-                <div className="ml-4">
-                  <p className="text-white-400">810</p>
-                </div>
-                <div className="avatar placeholder ml-16">
-                  <div className="bg-pink-500 text-neutral-content rounded-full border border-black w-6 h-6 ml-2">
-                    <span className="text-s font-display font-normal text-black">P</span>
-                  </div>
-                </div>
-                <div className="ml-4">
-                  <p className="text-white-400">2,600</p>
-                </div>
               </div>
-              <div className="flex flex-col gap-1 avatar placeholder">
-            </div>
-            <div className="flex flex-row items-center ml-6 -mt-24"> 
-              <h3 className="text-1xl font-display font-normal text-white ml-36 -mt-28">Tasks completed</h3>
-              <h4 className="text-1xl font-display font-normal text-white ml-36 -mt-28">Projects involved</h4>
-              <h5 className="text-1xl font-display font-normal text-white ml-36 -mt-28">Total earned</h5>
-            </div>
-            <div className="flex flex-row items-center gap-1 ml-44 -mt-14"> 
-              <p className="text-white-400">8</p>
-              <div className="flex flex-row items-center gap-1 ml-56 -mt-8"> 
-              <button className="btn btn-info mt-16 px-4 py-4"></button>
-              <button className="btn btn-success mt-16 px-4 py-4"></button>
-              </div>
-              <p className="text-white-400 ml-40 mt-8 mr-auto">1,600 USDC</p>
             </div>
           </div>
-        </div>
-        <h1 className="text-2xl font-display mt-4 mb-1">Working on</h1>
-        <div className="flex flex-row gap-1 my-4">
-          <TaskCard
-            title="Latte Protocol"
-            description="New Component Implementation"/>
+          <h1 className="text-2xl font-display mt-4 mb-1">Working on</h1>
+          <div className="flex flex-row gap-1 my-4">
+            <TaskCard
+              title="Latte Protocol"
+              description="New Component Implementation"
+            />
+          </div>
         </div>
       </div>
     </div>
